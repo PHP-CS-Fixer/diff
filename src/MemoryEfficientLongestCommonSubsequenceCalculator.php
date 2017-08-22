a04c32139a187c0401dcd4cd9832ebd59d4faa6a
SpacePossum <possumfromspace@gmail.com>
2017-08-22 15:10:04 +0200

    Switch NS

--- src/MemoryEfficientLongestCommonSubsequenceCalculator.php
+++ src/MemoryEfficientLongestCommonSubsequenceCalculator.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace SebastianBergmann\Diff;
+namespace PhpCsFixer\Diff;
 
 final class MemoryEfficientLongestCommonSubsequenceCalculator implements LongestCommonSubsequenceCalculator
 {
