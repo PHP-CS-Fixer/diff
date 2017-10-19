f0ef6133d674137e902fdf8a6f2e8e97e14a087b
SpacePossum <possumfromspace@gmail.com>
2017-10-19 11:10:07 +0200

    Backport GeckoPackages/DiffOutputBuilder

---
+++ tests/GeckoPackages/DiffOutputBuilder/Tests/ConfigurationExceptionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the GeckoPackages.
+ *
+ * (c) GeckoPackages https://github.com/GeckoPackages
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Diff\GeckoPackages\DiffOutputBuilder\Tests;
+
+use PhpCsFixer\Diff\GeckoPackages\DiffOutputBuilder\ConfigurationException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author SpacePossum
+ *
+ * @internal
+ */
+final class ConfigurationExceptionTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $e = new ConfigurationException('test', 'A', 'B');
+
+        $this->assertSame(0, $e->getCode());
+        $this->assertNull($e->getPrevious());
+        $this->assertSame('Option "test" must be A, got "string#B".', $e->getMessage());
+    }
+}
